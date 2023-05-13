75d9c8716a0b2519b69b27b69cb9b8d1539ec385
tonis <tonisives@gmail.com>
2023-05-13 09:28:51 +0700

    add item in the test script

--- python/run.py
+++ python/run.py
@@ -8,7 +8,6 @@ openai_ef = embedding_functions.OpenAIEmbeddingFunction(
                 model_name="text-embedding-ada-002"
             )
 
-
 client = chromadb.Client(Settings(chroma_api_impl="rest",
                                         chroma_server_host="localhost",
                                         chroma_server_http_port="8000"
@@ -16,23 +15,27 @@ client = chromadb.Client(Settings(chroma_api_impl="rest",
 
 try:
   collection = client.get_collection(name="my_collection", embedding_function=openai_ef)
-  # 2023-05-13 01:40:35 INFO     uvicorn.access  172.21.0.1:46890 - "GET /api/v1/collections/my_collection HTTP/1.1" 500
-  # 2023-05-13 01:40:35 WARNING  chromadb.api.models.Collection No embedding_function provided, using default embedding function: SentenceTransformerEmbeddingFunction
-  # 2023-05-13 01:40:35 ERROR    chromadb.server.fastapi Unable to load weights from pytorch checkpoint file for '/root/.cache/torch/sentence_transformers/sentence-transformers_all-MiniLM-L6-v2/pytorch_model.bin' at '/root/.cache/torch/sentence_transformers/sentence-transformers_all-MiniLM-L6-v2/pytorch_model.bin'. If you tried to load a PyTorch model from a TF 2.0 checkpoint, please set from_tf=True.
-  # T
-  print(f"existing collection {collection}")
+  print(f"existing collection: {collection}")
 except:
   collection = None
 
 if collection is None:
+  print("existing is None, create a new collection")
   collection = client.create_collection(name="my_collection", embedding_function=openai_ef)
 
-  new_item = openai_ef(["hello world"])
-  print(f"add {item}")
-  collection.add_item(item=new_item, id="id1")
-  # collection.add_item(item=item, id="id1")
-
 count = collection.count()
+
+if count == 0:
+  documents = ["hello world"]
+  embeddings = openai_ef(documents)
+  print(f"add embeddings: {embeddings.count()}")
+  collection.add(
+    documents=documents,
+    embeddings=embeddings,
+    metadatas=[{"name": "hello world"}],
+    ids=["id1"]
+  )
+
 print(f"count {count}")
 
 
