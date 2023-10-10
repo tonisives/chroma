38b635c0ba66dc8ec4618e410d3d79d1205efefe
tonisives <tonisives@gmail.com>
2023-10-10 20:05:17 +0700

    commit

--- ts/src/deleteCollection.ts
+++ ts/src/deleteCollection.ts
@@ -4,17 +4,19 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { ddbDeleteEmbFromAllContests } from "./ddb.js";
 
 let { client } = config
-let withDdb = false
+let withDdb = true
+
+
 
 let collections = [
   // contest - will remove em_stored from contests_2
   // `ah-00000000-${embNames.adaRecursive500}-2023-06-Index`,
 
   // -findings end: will delete only {embtype} c_embs_s from ddb finding_7 contests row
-  // "ah-00000000-85ab-findings",
+  "ah-00000000-f059-findings",
   // "ah-00000000-fc9d-findings",
   // "ah-00000000-7f50-findings",
-  "ah-00000000-8a3f-2023-09-maia-dao-ulysses"
+  // "ah-00000000-8a3f-2023-09-maia-dao-ulysses"
   // unused cohere (for now)
   // "ah-00000000-88a2-findings",
   // "ah-00000000-7722-findings"
@@ -24,23 +26,15 @@ let collections = [
 ]
 
 for (let collectionName of collections) {
-  console.log(`deleting collection ${collectionName}`);
+  console.log(`deleting collection ${collectionName} with DDB ${withDdb}`);
 
-  // await client.deleteCollection({ name: collectionName })
-  // continue
+  await client.deleteCollection({ name: collectionName })
 
-  let collection = await client.getOrCreateCollection({ name: collectionName })
-
-  await collection.delete({
-    where: {
-      type: {
-        $gte: 2
-      }
-    }
-  }).catch(e => {
+  /* let collection = await client.getOrCreateCollection({ name: collectionName })
+  await collection.delete({}).catch(e => {
     console.log(`didn't delete from chroma: ${e}`)
     process.exit(1)
-  })
+  }) */
 
   if (!withDdb) continue
 
